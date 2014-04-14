8a17ba9446cb45f5ee7483dd8008e30abad6c331
Guilherme Hashioka <memo@innku.com>
2014-04-14 18:00:15 -0500

    partials refactoring

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -25,7 +25,7 @@ describe "Authentication" do
 
       it { should have_title(user.name) }
       it { should have_link('Users',       href: users_path) }
-      it { should have_link('Profile',     href: user_path(user)) 
+      it { should have_link('Profile',     href: user_path(user))} 
       it { should have_link('Settings',    href: edit_user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
@@ -35,8 +35,8 @@ describe "Authentication" do
           it { should have_link('Sign in') }
       end 
     end
-
   end
+
   describe "authorization" do
 
     describe "for non-signed-in users" do
@@ -53,6 +53,7 @@ describe "Authentication" do
           before { patch user_path(user) }
           specify { expect(response).to redirect_to(signin_path) }
         end
+
         describe "visiting the user index" do
           before { visit users_path }
           it { should have_title('Sign in') }
